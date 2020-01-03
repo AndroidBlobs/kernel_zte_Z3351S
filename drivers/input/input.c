70ea2ab343f665d725ebb7fbf2e6deeb35a2d4df
ShivamKumarJha <jha.shivam3@gmail.com>
2020-01-03 21:51:54 +0000

    Add input modifications
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- drivers/input/input.c
+++ drivers/input/input.c
@@ -247,7 +247,7 @@ static int input_handle_abs_event(struct input_dev *dev,
 	if (pold) {
 		*pval = input_defuzz_abs_event(*pval, *pold,
 						dev->absinfo[code].fuzz);
-		if (*pold == *pval)
+		if (*pold == *pval && code != ABS_DISTANCE)
 			return INPUT_IGNORE_EVENT;
 
 		*pold = *pval;
